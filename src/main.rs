65e62629a4d7a85b1c8765e21ddac76f214a7175
Olli Varila <olli.varila@gmail.com>
2024-03-02 23:03:27 +0200

    setup stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::{
-    fs::{DirEntry, ReadDir},
-    io::Read,
-    rc::Rc,
-};
+use anyhow::Result;
+use clap::{command, Parser};
+mod setup;
 
-use clap::Parser;
-use toml::Table;
-
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    let programs = parse_programs(args);
+    let programs = setup::run(&args)?;
+    dbg!(programs);
+
+    Ok(())
 }
 
 #[derive(Parser, Debug)]
@@ -20,39 +18,7 @@ struct Args {
         short,
         long,
         help = "Path where dotfiles are cloned into",
-        default_value = "~"
+        default_value = "~/dotfiles"
     )]
     path: String,
-}
-
-struct ProgramConfig {
-    dir_entry: DirEntry,
-    options: ProgramOptions,
-}
-
-struct ProgramOptions {
-    name: String,
-    target: String,
-    is_installed_cmd: Option<String>,
-}
-
-fn parse_programs(args: Args) -> Option<Vec<ProgramConfig>> {
-    const S: &str = include_str!("../mage-example.toml");
-    let table = S.parse::<Table>().unwrap();
-    let dotfiles = clone_repo(&args.repo_url, &args.path)
-        .expect("failed to clone repo")
-        .map(|item| item.expect("failed to extract dir entry"))
-        .map(|item| {
-            item.file_name()
-                .to_str()
-                .unwrap()
-                .trim_end_matches(".*")
-                .to_string()
-        })
-        .collect::<Vec<_>>();
-    todo!()
-}
-
-fn clone_repo(url: &str, path: &str) -> Option<ReadDir> {
-    todo!()
 }
---
+++ src/setup.rs
@@ -0,0 +1,146 @@
+use crate::Args;
+use anyhow::{bail, Context, Ok, Result};
+use std::{
+    fs::{self, ReadDir},
+    path::PathBuf,
+};
+use toml::Table;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ProgramOptions {
+    name: String,
+    path: PathBuf,
+    target_path: String,
+    is_installed_cmd: Option<String>,
+}
+
+impl ProgramOptions {
+    fn new(
+        name: String,
+        path: PathBuf,
+        target_path: String,
+        is_installed_cmd: Option<String>,
+    ) -> Self {
+        ProgramOptions {
+            name,
+            path,
+            target_path,
+            is_installed_cmd,
+        }
+    }
+}
+
+pub fn run(args: &Args) -> Result<Vec<ProgramOptions>> {
+    let cloned_repo = clone_repo(&args.repo_url, &args.path)?;
+    parse_dotfiles(cloned_repo)
+}
+
+fn magefile(path: PathBuf) -> Result<Table> {
+    let magefile = fs::read_to_string(path)?;
+    toml::from_str(&magefile).context("Failed to parse magefile")
+}
+
+fn parse_dotfiles(dotfiles: ReadDir) -> Result<Vec<ProgramOptions>> {
+    let mut mage_config: Option<Table> = None;
+    let mut programs = vec![];
+
+    for item in dotfiles {
+        let entry = item.context("Failed to read dir entry")?;
+        if entry.file_name().to_str().unwrap().starts_with("magefile") {
+            mage_config = Some(magefile(entry.path())?);
+            continue;
+        }
+
+        let path = entry.path();
+        let key = path
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .context(format!("Failed to get filename for {}", path.display()))?
+            .to_string();
+
+        programs.push((key, path));
+    }
+
+    let mage_config = mage_config.context("No magefile found")?;
+
+    let mut result = vec![];
+
+    for (name, path) in programs.into_iter() {
+        let program = create_program_options(&mage_config, name, path)?;
+        result.push(program);
+    }
+
+    Ok(result)
+}
+
+fn create_program_options(magefile: &Table, name: String, path: PathBuf) -> Result<ProgramOptions> {
+    let program = magefile
+        .get(&name)
+        .context(format!("No entry for {}", name))?;
+
+    let target_path = program
+        .get("target_path")
+        .context(format!("No target_path for {}", name))?
+        .to_string();
+
+    let is_installed_cmd = program.get("is_installed_cmd").map(|cmd| cmd.to_string());
+
+    Ok(ProgramOptions::new(
+        name,
+        path,
+        target_path,
+        is_installed_cmd,
+    ))
+}
+
+fn clone_repo(url: &str, path: &str) -> Result<ReadDir> {
+    if PathBuf::from(path).exists() {
+        bail!("Path `{}` already exists", path)
+    }
+
+    std::process::Command::new("git")
+        .args(&["clone", url, path])
+        .output()
+        .context(format!("Failed to clone repo `{url}` into `{path}`"))?;
+
+    fs::read_dir(path).context(format!("Failed to read cloned repo from `{path}`"))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parse_dotfiles() {
+        fs::create_dir("/tmp/mage").expect("failed to create temp dir");
+
+        // let args = Args {
+        //     path: "/tmp/mage".to_string(),
+        //     repo_url: "".to_string(),
+        // };
+        let dotfiles = fs::read_dir("test-dotfiles").unwrap();
+        let programs = parse_dotfiles(dotfiles).unwrap();
+
+        fs::remove_dir_all("/tmp/mage").unwrap_or_default();
+        assert_eq!(programs.len(), 1);
+    }
+
+    #[test]
+    fn does_not_clone_if_path_exists() {
+        let result = clone_repo("empty", "test-dotfiles");
+        assert!(result.is_err());
+        let msg = result.unwrap_err().to_string();
+        assert_eq!(msg, "Path `test-dotfiles` already exists");
+    }
+
+    #[test]
+    fn test_clone_repo() {
+        fs::remove_dir_all("/tmp/mage").unwrap_or_default();
+        clone_repo("https://github.com/ollivarila/brainfckr.git", "/tmp/mage").unwrap();
+        let dir_exists = PathBuf::from("/tmp/mage").exists();
+        assert!(dir_exists);
+        fs::remove_dir_all("/tmp/mage").unwrap_or_default();
+    }
+}
